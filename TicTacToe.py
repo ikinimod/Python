0871ff25eaae33585642304395e54b4930ffc679
ikinimod <dominik.holm1998@gmail.com>
2024-04-12 14:50:21 +0200

    Game Field

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,12 +1,17 @@
 # This is a portfolio project. I will try to make a tictactoe game with python respectively obcejt orientated programming.
 print(""" Welcome to my Tic Tac Toe game. 
-       ________    
-      |__|__|__|
-      |__|__|__|
-      |__|__|__|
+       ___________    
+      |_1_|_2_|_3_|
+      |_4_|_5_|_6_|
+      |_7_|_8_|_9_|
 Enter first player name!""")
-#class X(self):
+class Player1(name):
+      self.name = ""
 
 
-#class O(self):
+class Player2(name):
+      self.name = ""
+      
+
+
 
