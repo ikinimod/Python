36daea63cb9cf4373af78380f41983f815bcd48f
ikinimod <dominik.holm1998@gmail.com>
2024-04-12 12:31:03 +0200

    Created Python File and Brainstorming file

---
+++ TicTacToe.py
@@ -0,0 +1,12 @@
+# This is a portfolio project. I will try to make a tictactoe game with python respectively obcejt orientated programming.
+print(""" Welcome to my Tic Tac Toe game. 
+       ________    
+      |__|__|__|
+      |__|__|__|
+      |__|__|__|
+Enter first player name!""")
+#class X(self):
+
+
+#class O(self):
+
