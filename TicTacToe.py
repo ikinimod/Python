a3ae96c782e5060ad1c728d6d402417d31ceaa76
ikinimod <dominik.holm1998@gmail.com>
2024-04-12 14:52:21 +0200

    Printing field

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,17 +1,11 @@
 # This is a portfolio project. I will try to make a tictactoe game with python respectively obcejt orientated programming.
-print(""" Welcome to my Tic Tac Toe game. 
-       ___________    
-      |_1_|_2_|_3_|
-      |_4_|_5_|_6_|
-      |_7_|_8_|_9_|
-Enter first player name!""")
-class Player1(name):
-      self.name = ""
 
+field = [" ",
+             "1","2","3",
+             "4","5","6",
+             "7","8","9"]
 
-class Player2(name):
-      self.name = ""
-      
-
-
-
+# field printing
+print (field[1] + "|" + field[2] + "|" + field[3] )
+print (field[4] + "|" + field[5] + "|" + field[6] )
+print (field[7] + "|" + field[8] + "|" + field[9] )
