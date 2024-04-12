354955598044e57125b59969f7a5964f33c83808
ikinimod <dominik.holm1998@gmail.com>
2024-04-12 14:54:59 +0200

    Added game field function

--- TicTacToe.py
+++ TicTacToe.py
@@ -9,3 +9,10 @@ field = [" ",
 print (field[1] + "|" + field[2] + "|" + field[3] )
 print (field[4] + "|" + field[5] + "|" + field[6] )
 print (field[7] + "|" + field[8] + "|" + field[9] )
+
+# Printing field function
+def print_field():
+    print (field[1] + "|" + field[2] + "|" + field[3] )
+    print (field[4] + "|" + field[5] + "|" + field[6] )
+    print (field[7] + "|" + field[8] + "|" + field[9] )
+print_field()
